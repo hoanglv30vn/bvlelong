c4f569fb27a96629ee728b36f4cf530200123c5f
hoanglv <hoanglv.30@gmail.com>
2021-10-31 16:40:15 +0700

    fix scale video hành khúc lê long

--- logic/hinhanh.js
+++ logic/hinhanh.js
@@ -2,7 +2,7 @@ main_lelong_butt_hinhanh_html = `
 <h2 class="title-center">Hình ảnh hoạt động của Lê Long</h2>
 
 <div class="video-itro">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/rWi-v4DkNyU?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe frameborder="0 " allowfullscreen="1 " allow="accelerometer; autoplay; clipboard-write; encrypted-media;  gyroscope; picture-in-picture " title="YouTube video player " width="100% " height="100% "  src="https://www.youtube.com/embed/rWi-v4DkNyU?controls=0"; ></iframe>
 
 </div>
 <div class="slider"></div>
